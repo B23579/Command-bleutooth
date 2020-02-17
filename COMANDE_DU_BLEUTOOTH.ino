3e740556c06947d5c6dd266d1f79edb5d6a067b6
B23579 <58120702+B23579@users.noreply.github.com>
2020-02-17 09:06:30 +0100

    Add files via upload

---
+++ COMANDE_DU_BLEUTOOTH.ino
@@ -0,0 +1,32 @@
+#include <SoftwareSerial.h>
+#define RX 4
+#define TX  2
+ SoftwareSerial BT(RX,TX);
+void setup() {
+  Serial.begin(9600);
+  BT.begin(9600);
+  pinMode(RX,INPUT);
+  pinMode(TX,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  char mot[20];
+  int i=0;
+  int dispo;
+  dispo=BT.available();
+  if(dispo>0){
+    while(dispo>0&&i<10){
+      mot[i]=BT.read();
+      delay(10);
+      dispo=BT.available();
+      i++;
+      
+    }
+    Serial.println(mot);
+    BT.write(mot);
+  // put your main code here, to run repeatedly:
+
+} 
+}
